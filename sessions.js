a3bd1d0e7c8e1043f5ee7fbd81a5e22b33b2fe8f
comfortaduloju40-alt <comfortaduloju40@gmail.com>
2026-07-04 04:56:55 +0100

    Create sessions.js

---
+++ sessions.js
@@ -0,0 +1,38 @@
+// Tracks each user's progress through the form
+const sessions = {};
+
+function getSession(chatId) {
+  return sessions[chatId] || null;
+}
+
+function createSession(chatId) {
+  sessions[chatId] = {
+    step: 'eventType',
+    data: {
+      eventType: '',
+      eventName: '',
+      hostName: '',
+      guestName: '',
+      date: '',
+      time: '',
+      venue: '',
+      dresscode: '',
+      rsvp: ''
+    }
+  };
+  return sessions[chatId];
+}
+
+function updateSession(chatId, nextStep, fieldData) {
+  if (!sessions[chatId]) return;
+  sessions[chatId].step = nextStep;
+  if (fieldData) {
+    Object.assign(sessions[chatId].data, fieldData);
+  }
+}
+
+function clearSession(chatId) {
+  delete sessions[chatId];
+}
+
+module.exports = { getSession, createSession, updateSession, clearSession };
